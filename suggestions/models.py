e94de010cdf045f4e44715c330dd31d2499e5a68
Aly Yakan <alyyakan@hotmail.com>
2015-08-31 14:28:41 +0300

    Added models and views

--- suggestions/models.py
+++ suggestions/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import (
+    GenericForeignKey)
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class ObjectView(models.Model):
+
+    """ Represents a user that viewed an object's page """
+    user = models.ForeignKey(User)
+    content_type = models.ForeignKey(ContentType, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+
+        """ A pair of user,object is unique, to avoid duplication """
+        unique_together = ('user', 'content_object',)
+
+
+class ObjectViewDictionary(models.Model):
+
+    """
+    Represents an object that a user visited before visiting the current one
+    """
+    current_content_type = models.ForeignKey(ContentType, null=True)
+    current_object_id = models.PositiveIntegerField(null=True)
+    current_object = GenericForeignKey(
+        'current_content_type', 'current_object_id')
+
+    visited_before_content_type = models.ForeignKey(ContentType, null=True)
+    visited_before_object_id = models.PositiveIntegerField(null=True)
+    visited_before_object = GenericForeignKey(
+        'visited_before_content_type', 'visited_before_object_id')
+
+    visits = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = ('current_object', 'visited_before_object',)
--- suggestions/views.py
+++ suggestions/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from .models import ObjectView, ObjectViewDictionary
 
-# Create your views here.
+
+def update_suggestions_dictionary(request, object):
+    if request.user.is_authenticated():
+        user = request.user
+        try:
+            ObjectView.objects.get(user=user, object=object)
+        except:
+            ObjectView.objects.create(user=user, object=object)
+        viewed = ObjectView.objects.filter(user=user).exclude(
+            content_object=object)
+        if viewed:
+            for obj in viewed:
+                if type(object) == type(obj):
+                    try:
+                        visited = ObjectViewDictionary.objects.get(
+                            current_object=object,
+                            visited_before_object=obj.content_object)
+                        count = visited.visits + 1
+                        visited.visits = count
+                        visited.save()
+                    except:
+                        ObjectViewDictionary.objects.create(
+                            current_recipe=object,
+                            visited_before=obj.content_object)
+    return
+
+
+def get_suggestions_with_size(object, size):
+    try:
+        return ObjectViewDictionary.objects.filter(
+            current_object=object).extra(order_by=['-visits'])[:size]
+    except:
+        return ObjectViewDictionary.objects.filter(
+            current_object=object).extra(order_by=['-visits'])
+
+
+def get_suggestions(object):
+    return ObjectViewDictionary.objects.filter(
+        current_object=object).extra(order_by=['-visits'])
