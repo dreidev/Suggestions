0c602088374bf792eac8e49c2e6db1c6982b7d2d
ranael-garem <ranaelgarem781994@gmail.com>
2015-09-13 09:02:23 +0200

    fixed update_suggestions_dictionary in views.py

--- suggestions/views.py
+++ suggestions/views.py
@@ -1,29 +1,33 @@
 from .models import ObjectView, ObjectViewDictionary
+from django.contrib.contenttypes.models import ContentType
 
 
 def update_suggestions_dictionary(request, object):
     if request.user.is_authenticated():
         user = request.user
+        content_type = ContentType.objects.get_for_model(type(object))
         try:
-            ObjectView.objects.get(user=user, object=object)
+            ObjectView.objects.get(
+                user=user, object_id=object.id, content_type=content_type)
         except:
-            ObjectView.objects.create(user=user, object=object)
-        viewed = ObjectView.objects.filter(user=user).exclude(
-            content_object=object)
+            ObjectView.objects.create(user=user, content_object=object)
+        viewed = ObjectView.objects.filter(user=user)
         if viewed:
             for obj in viewed:
-                if type(object) == type(obj):
-                    try:
-                        visited = ObjectViewDictionary.objects.get(
+                try:
+                    visited = ObjectViewDictionary.objects.get(
+                        current_object_id=object.id,
+                        current_content_type=content_type,
+                        visited_before_object_id=obj.object_id,
+                        visited_before_content_type=obj.content_type)
+                    count = visited.visits + 1
+                    visited.visits = count
+                    visited.save()
+                except:
+                    if object.id != obj.object_id:
+                        ObjectViewDictionary.objects.create(
                             current_object=object,
                             visited_before_object=obj.content_object)
-                        count = visited.visits + 1
-                        visited.visits = count
-                        visited.save()
-                    except:
-                        ObjectViewDictionary.objects.create(
-                            current_recipe=object,
-                            visited_before=obj.content_object)
     return
 
 
