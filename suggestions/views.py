b1c04d39f7267e9aea2258ca6c7e2a5f70d3088c
Aly Yakan <alyyakan@hotmail.com>
2015-09-20 10:24:18 +0200

    Dictionary is updated with an entry and its reverse

--- suggestions/views.py
+++ suggestions/views.py
@@ -6,9 +6,9 @@ def update_suggestions_dictionary(request, object):
     """
     Updates the suggestions' dictionary for an object upon visiting its page
     """
+    content_type = ContentType.objects.get_for_model(type(object))
     if request.user.is_authenticated():
         user = request.user
-        content_type = ContentType.objects.get_for_model(type(object))
         try:
             # Check if the user has visited this page before
             ObjectView.objects.get(
@@ -19,25 +19,41 @@ def update_suggestions_dictionary(request, object):
         viewed = ObjectView.objects.filter(user=user)
         if viewed:
             for obj in viewed:
-                try:
-                    # Check if there is an entry in the dictionary
-                    # and update the occurence
-                    visited = ObjectViewDictionary.objects.get(
-                        current_object_id=object.id,
-                        current_content_type=content_type,
-                        visited_before_object_id=obj.object_id,
-                        visited_before_content_type=obj.content_type)
-                    count = visited.visits + 1
-                    visited.visits = count
-                    visited.save()
-                except:
+                if not exists_in_dictionary(obj, obj.content_type, object, content_type, True):
                     # Create an entry if it's non existent
                     if object.id != obj.object_id:
                         if content_type == obj.content_type:
                             ObjectViewDictionary.objects.create(
                                 current_object=object,
                                 visited_before_object=obj.content_object)
+                        if not exists_in_dictionary(obj, obj.content_type, object, content_type, False):
+                            ObjectViewDictionary.objects.create(
+                                current_object=obj.content_object,
+                                visited_before_object=object)
+
+    # else:
+    #     try:
+    #         visited = request.session['visited']
+    #     except:
+    #         visited = []
+    #         request.session = visited
     return
+
+
+def exists_in_dictionary(object, content_type, obj, obj_content_type, update):
+    try:
+        visited = ObjectViewDictionary.objects.get(
+                current_object_id=object.id,
+                current_content_type=content_type,
+                visited_before_object_id=obj.object_id,
+                visited_before_content_type=obj_content_type)
+        if update:
+            count = visited.visits + 1
+            visited.visits = count
+            visited.save()
+        return True
+    except:
+        return False
 
 
 def get_suggestions_with_size(object, size):
